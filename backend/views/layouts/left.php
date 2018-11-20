9043c7ac08a5f2d8a583ff3e5e8d6fcfe6b7f3ca
bjlag <vlad.duplin@ya.ru>
2018-11-20 16:56:26 +0300

    Backend. Выделение пункта меню

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -18,7 +18,7 @@
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
+                <span class="input-group-btn">
                 <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
               </span>
@@ -28,11 +28,16 @@
 
         <?= dmstr\widgets\Menu::widget(
             [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
                 'items' => [
                     ['label' => 'Меню', 'options' => ['class' => 'header']],
 
-                    ['label' => 'Пользователи', 'icon' => 'user', 'url' => ['/user']],
+                    [
+                        'label' => 'Пользователи',
+                        'icon' => 'user',
+                        'url' => ['/user'],
+                        'active' => $this->context->id === 'user'
+                    ],
 //                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
 //                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
 //                    [
